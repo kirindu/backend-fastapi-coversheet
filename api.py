621b0d97620d1aaeb7e6d046e8bf2bd849f275f9
Roberto Payan <kirindu3@gmail.com>
2025-04-03 17:13:13 -0600

    avance de metodos

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status, File, UploadFile, Form, HTTPException
 from fastapi.responses import JSONResponse, HTMLResponse, FileResponse, StreamingResponse
+from datetime import datetime
 import os
 import shutil
 from uuid import uuid4
@@ -9,9 +10,24 @@ api = APIRouter()
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+# Los métodos para enviar fotos se esperan que sean enviados como multipart/form-data
+
+@api.post("/subida/", status_code=status.HTTP_200_OK)
+async def subida(imagen: UploadFile = File(...)):
+    if getExtension(imagen.content_type)=="no":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El archivo debe ser JPG o PNG")
+    nombre = f"{datetime.timestamp(datetime.now())}{getExtension(imagen.content_type)}" # Le damos un nombre a la imagen
+    file_name = os.getcwd() + "/uploads/" + nombre
+    with open(file_name, 'wb+') as f:
+        f.write(imagen.file.read())
+        f.close()
+    return {"mensaje": imagen}
+    # Si queremos mostrar la imagen que subimos
+    # return FileResponse(os.getcwd()+"/uploads/" + nombre)
 
 
-@api.post("/upload/")
+
+@api.post("/upload/", tags=['Loads'])
 async def upload_image(
     nombre: str = Form(...),
     direccion: str = Form(...),
@@ -41,3 +57,11 @@ async def upload_image(
             status_code=500,
             content={"error": str(e)}
         )
+
+def getExtension(content_type):
+    if content_type == "image/png":
+        return ".png"
+    elif content_type == "image/jpg":
+        return ".jpg"
+    else:
+        return "no"
--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from api import api
 
 app = FastAPI()
 
+# Configuracion para el docs
+app.title = "Rest-Api CoverSheet"
+app.version = "1.0.0"
+
 app.include_router(api)
 
